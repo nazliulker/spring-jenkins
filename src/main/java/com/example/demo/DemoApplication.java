bfa13f93acd43520c97026b9fd0c35ad6abd319d
nazliulker <43544163+nazliulker@users.noreply.github.com>
2024-05-02 12:17:17 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,16 @@
 package com.example.demo;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@GetMapping("/")
+	public String displayMessage(){
+		return "Hello World :)";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
