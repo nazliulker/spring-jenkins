e1b1772022287457449da182080f79305bbdb97f
nazliulker <43544163+nazliulker@users.noreply.github.com>
2024-05-02 12:32:23 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	@GetMapping("/")
